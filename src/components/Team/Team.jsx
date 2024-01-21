cc20eb079bb93030b37a271133a5fb669da9ff74
amazio <amaziosoftware@gmail.com>
2024-01-21 12:21:28 -0800

    Click to select team

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,11 +1,12 @@
 import './Team.css';
 
-export default function Team({ team, isActive, locked }) {
+export default function Team({ team, setActiveTeam, isActive, locked }) {
   return (
     <article
       className={`Team${isActive && !locked ? ' active' : ''}`}
-      style={{gridArea: `team-${team.num}`}
-    }>
+      style={{gridArea: `team-${team.num}`}}
+      onClick={() => setActiveTeam(team)}
+    >
       <div>Team {team.num}</div>
       <h3>{team.player1}</h3>
       <h3>{team.player2}</h3>
--- src/components/TeamGrid/TeamGrid.jsx
+++ src/components/TeamGrid/TeamGrid.jsx
@@ -3,7 +3,13 @@ import Team from '../Team/Team';
 import Bidder from '../Bidder/Bidder';
 
 export default function TeamGrid({ teams, activeTeam, setActiveTeam, locked }) {
-  const teamCards = teams.map((t) => <Team team={t} isActive={t === activeTeam} locked={locked} key={t.num} />);
+  const teamCards = teams.map((t) => <Team
+    team={t}
+    setActiveTeam={setActiveTeam}
+    isActive={t === activeTeam}
+    locked={locked}
+    key={t.num}
+  />);
   return (
     <section className="TeamGrid">
       {teamCards}
